56224b289c4204cddf2016d432b9a4617b02e35b
Mark Helotie <43053988+mark-helotie@users.noreply.github.com>
2023-10-04 20:01:04 -0400

    Add files via upload

--- static/js/logic.js
+++ static/js/logic.js
@@ -270,68 +270,73 @@ city_data = [
   
   // Function to fetch events in a city using Ticketmaster API
   function getEventsInCity(id) {
-    const apiKey = 'S2v9Md44UbLI7UVMA583AjbIZ4dPB5tu';
-
-    // Define the Ticketmaster API URL to fetch events in the city
-    const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?city=${cityName}&apikey=${apiKey}&size=10`;
-
-    fetch(apiUrl)
-        .then(response => response.json())
-        .then(data => {
-            // Process the data and display event details on the map as markers
+      const apiKey = 'S2v9Md44UbLI7UVMA583AjbIZ4dPB5tu';
+  
+      // Define the Ticketmaster API URL to fetch events in the city
+      const apiUrl = `https://app.ticketmaster.com/discovery/v2/events.json?marketId=${id}&apikey=${apiKey}&size=10`;
+  
+      fetch(apiUrl)
+          .then(response => response.json())
+          .then(data => {
+  
+              // Process the data and display event details on the map as markers
             var events = data._embedded.events;
-            for (let i = 0; i < events.length; i++) {
+              for (let i = 0; i < events.length; i++) {
                 var event = events[i];
                 var eventName = event.name;
                 var eventVenue = event._embedded.venues[0].name;
                 var eventDate = event.dates.start.localDate;
                 var eventLat = event._embedded.venues[0].location.latitude;
                 var eventLng = event._embedded.venues[0].location.longitude;
-
-                // Create a marker for the event
+  
+                  // Create a marker for the event
                 var eventMarker = L.marker([eventLat, eventLng]).addTo(myMap);
-
-                // Add a popup with event details to the marker
-                eventMarker.bindPopup(`<h1>${eventName}</h1><p>Venue: ${eventVenue}</p><p>Date: ${eventDate}</p>`);
-
-                // Add the event marker to a separate layer group for events
-                eventMarker.addTo(eventMarkerGroup);
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching events:', error);
-        });
-}
-
-// Create a layer group for event markers
-var eventMarkerGroup = L.layerGroup();
-
-// Loop through the cities array and create one marker for each city object.
-for (let i = 0; i < city_data.length; i++) {
-    var marker = L.marker([city_data[i].lat, city_data[i].lng], {
-        icon: L.divIcon({ className: 'custom-div-icon' })
-    }).addTo(cityMarkerGroup);
-
-    // Add a mouseover event to show the city name when hovering over the marker.
-    marker.on('mouseover', function () {
-        marker.bindPopup(`<h1>${city_data[i].name}</h1>`).openPopup();
-    });
-
-    // Add a click event to fetch and display events in the city when clicking on the marker.
-    marker.on('click', function () {
-        // Clear previous event markers from the map
-          myMap.setView([city_data[i].lat, city_data[i].lng], 9)
-          eventMarkerGroup.eachLayer(function (layer) {
-              if (layer !== myMap) {
-                  eventMarkerGroup.removeLayer(layer);
+  
+                  // Add a popup with event details to the marker
+                  eventMarker.bindPopup(`<h1>${eventName}</h1><p>Venue: ${eventVenue}</p><p>Date: ${eventDate}</p>`);
+  
+                  // Add the event marker to a separate layer group for events
+                  eventMarker.addTo(eventMarkerGroup);
               }
+          })
+          .catch(error => {
+              console.error('Error fetching events:', error);
           });
-        eventMarkerGroup.clearLayers();
-
-        // Call the function to fetch and display events
+  }
+  
+  // Create a layer group for event markers
+  var eventMarkerGroup = L.layerGroup();
+  
+  // Loop through the cities array and create one marker for each city object.
+  for (let i = 0; i < city_data.length; i++) {
+    var marker = L.marker([city_data[i].lat, city_data[i].lng], {
+          icon: L.divIcon({ className: 'custom-div-icon' })
+      }).addTo(cityMarkerGroup);
+  
+      // Add a mouseover event to show the city name when hovering over the marker.
+      marker.on('mouseover', function () {
+          marker.bindPopup(`<h1>${city_data[i].name}</h1>`).openPopup();
+      });
+  
+      // Add a click event to fetch and display events in the city when clicking on the marker.
+      marker.on('click', function () {
+  
+          // Clear previous event markers from the map
+          eventMarkerGroup.clearLayers();
+  
+          myMap.setView([city_data[i].lat, city_data[i].lng], 9)
+  
+          });
+  
+          eventMarkerGroup.clearLayers();
+  
+         // Call the function to fetch and display events
           getEventsInCity(city_data[i].id);
           
           // Add back the clicked city marker to the map.
           eventMarkerGroupMarkerGroup.addLayer(marker);
-    });
-}
+  
+          
+          
+      });
+  }
