2fc92b172e28748789411a7aa4f6e2a4ce5019fb
Mark Helotie <43053988+mark-helotie@users.noreply.github.com>
2023-10-04 23:48:39 -0400

    Update JS files

---
+++ static/js/array.js
@@ -0,0 +1,261 @@
+// Cities array data created
+city_data = [
+    {
+        'id': 1,
+        'name': 'Birmingham',
+        'lat': 33.53894154,
+        'lng': -86.81664397
+    },
+    {
+        'id': 2,
+        'name': 'Charlotte',
+        'lat': 35.23466349,
+        'lng': -80.84030215
+    },
+    {
+        'id': 3,
+        'name': 'Chicago',
+        'lat': 41.90491852,
+        'lng': -87.66652212
+    },
+    {
+        'id': 4,
+        'name': 'Cincinnati & Dayton',
+        'lat': 39.48953605,
+        'lng': -84.32655339
+    },
+    {
+        'id': 5,
+        'name': 'Dallas - Fort Worth',
+        'lat': 32.80091837,
+        'lng': -97.02638929
+    },
+    {
+        'id': 6,
+        'name': 'Denver',
+        'lat': 39.82798139,
+        'lng': -104.997396
+    },
+    {
+        'id': 7,
+        'name': 'Detroit',
+        'lat': 42.32642373,
+        'lng': -83.05239103
+    },
+    {
+        'id': 9,
+        'name': 'Grand Rapids',
+        'lat': 42.97259808,
+        'lng': -85.67133373
+    },
+    {
+        'id': 10,
+        'name': 'Atlanta',
+        'lat': 33.82387988,
+        'lng': -84.42667883
+    },
+    {
+        'id': 11,
+        'name': 'Boston',
+        'lat': 42.34775132,
+        'lng': -70.88933318
+    },
+    {
+        'id': 12,
+        'name': 'Cleveland & Youngstown',
+        'lat': 41.2878937,
+        'lng': -81.12773068
+    },
+    {
+        'id': 13,
+        'name': 'Columbus',
+        'lat': 39.96545177,
+        'lng': -83.00794793
+    },
+    {
+        'id': 14,
+        'name': 'Las Vegas',
+        'lat': 36.25641193,
+        'lng': -115.1604067
+    },
+    {
+        'id': 15,
+        'name': 'Miami',
+        'lat': 25.84611802,
+        'lng': -80.20243214
+    },
+    {
+        'id': 16,
+        'name': 'Minneapolis - St. Paul',
+        'lat': 44.98821329,
+        'lng': -93.19925812
+    },
+    {
+        'id': 17,
+        'name': 'Orlando',
+        'lat': 28.63858177,
+        'lng': -81.37843688
+    },
+    {
+        'id': 18,
+        'name': 'Philadelphia',
+        'lat': 40.0418458,
+        'lng': -75.1568349
+    },
+    {
+        'id': 19,
+        'name': 'Pittsburgh',
+        'lat': 40.44254943,
+        'lng': -79.99646373
+    },
+    {
+        'id': 20,
+        'name': 'San Diego',
+        'lat': 32.71363697,
+        'lng': -117.14455073
+    },
+    {
+        'id': 21,
+        'name': 'Tampa',
+        'lat': 28.04715906,
+        'lng': -82.46525417
+    },
+    {
+        'id': 22,
+        'name': 'Houston',
+        'lat': 29.92299529,
+        'lng': -95.28580873
+    },
+    {
+        'id': 23,
+        'name': 'Indianapolis',
+        'lat': 39.82306606,
+        'lng': -86.00887087
+    },
+    {
+        'id': 25,
+        'name': 'Jacksonville',
+        'lat': 30.41365267,
+        'lng': -81.67405732
+    },
+    {
+        'id': 26,
+        'name': 'Kansas City',
+        'lat': 39.23727452,
+        'lng': -94.64727307
+    },
+    {
+        'id': 27,
+        'name': 'Los Angeles',
+        'lat': 34.04906332,
+        'lng': -118.23538502
+    },
+    {
+        'id': 28,
+        'name': 'Louisville & Lexington',
+        'lat': 38.20794155,
+        'lng': -85.0848732
+    },
+    {
+        'id': 29,
+        'name': 'Memphis',
+        'lat': 35.20590353,
+        'lng': -90.01646998
+    },
+    {
+        'id': 30,
+        'name': 'Milwaukee',
+        'lat': 43.91876064,
+        'lng': -88.89034085
+    },
+    {
+        'id': 31,
+        'name': 'Nashville & Knoxville',
+        'lat': 36.16184045,
+        'lng': -85.72407445
+    },
+    {
+        'id': 34,
+        'name': 'New Orleans',
+        'lat': 30.02644523,
+        'lng': -90.08208773
+    },
+    {
+        'id': 35,
+        'name': 'New York City',
+        'lat': 40.81671621,
+        'lng': -74.07390105
+    },
+    {
+        'id': 36,
+        'name': 'Phoenix & Tucson',
+        'lat': 33.00440348,
+        'lng': -111.7056077
+    },
+    {
+        'id': 37,
+        'name': 'Portland',
+        'lat': 45.68777482,
+        'lng': -122.686847
+    },
+    {
+        'id': 38,
+        'name': 'Raleigh/Durham',
+        'lat': 35.86001172,
+        'lng': -78.64521226
+    },
+    {
+        'id': 39,
+        'name': 'St. Louis',
+        'lat': 38.78239146,
+        'lng': -90.24327953
+    },
+    {
+        'id': 40,
+        'name': 'San Antonio & Austin',
+        'lat': 29.85648388,
+        'lng': -97.98860813
+    },
+    {
+        'id': 41,
+        'name': 'San Francisco',
+        'lat': 38.12188517,
+        'lng': -122.06498582
+    },
+    {
+        'id': 42,
+        'name': 'Seattle',
+        'lat': 47.79366735,
+        'lng': -122.4920295
+    },
+    {
+        'id': 45,
+        'name': 'Salt Lake City',
+        'lat': 40.75989882,
+        'lng': -111.90331301
+    },
+    {
+        'id': 47,
+        'name': 'Washington DC & Maryland',
+        'lat': 39.01199167,
+        'lng': -76.99621831
+    },
+    {
+        'id': 49,
+        'name': 'Hawaii',
+        'lat': 21.37574116,
+        'lng': -157.8530221
+    },
+    {
+        'id': 52,
+        'name': 'Omaha',
+        'lat': 41.35239372,
+        'lng': -96.06223062
+    },
+    {
+        'id': 125,
+        'name': 'Oklahoma City',
+        'lat': 35.60134994,
+        'lng': -97.47857292
+    },
+  ]
